cfb23438670b61e1c179f8f4df0dd34ccb7ea0ba
NoteXYX <707997228@qq.com>
2019-02-20 11:24:01 +0800

    ok

--- scripts/train_sec2vec_model.py
+++ scripts/train_sec2vec_model.py
@@ -1,19 +1,56 @@
+import gensim
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+import collections
+import smart_open
+import random
 import logging
-import sys
 import os
-from gensim.models.word2vec import Sent2Vec, LineSentence
+import sys
+import multiprocessing
 
-logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
-logging.info("running %s" % " ".join(sys.argv))
+# Set file names for train and test data
+# test_data_dir = '{}'.format(os.sep).join([gensim.__path__[0], 'test', 'test_data'])
+# lee_train_file = test_data_dir + os.sep + 'lee_background.cor'
+# lee_test_file = test_data_dir + os.sep + 'lee.cor'
+def read_corpus(fname, tokens_only=False):
+    with smart_open.smart_open(fname, encoding="utf-8") as f:
+        for i, line in enumerate(f):
+            if tokens_only:
+                yield gensim.utils.simple_preprocess(line)
+            else:
+                # For training data, add tags
+                yield TaggedDocument(gensim.utils.simple_preprocess(line), [i])
 
-input_file = 'test.txt'
-model = Word2Vec(LineSentence(input_file), size=100, window=5, sg=0, min_count=5, workers=8)
-model.save(input_file + '.model')
-model.save_word2vec_format(input_file + '.vec')
+if __name__ == '__main__':
+    program = os.path.basename(sys.argv[0])
+    logger = logging.getLogger(program)
 
-sent_file = 'sent.txt'
-model = Sent2Vec(LineSentence(sent_file), model_file=input_file + '.model')
-model.save_sent2vec_format(sent_file + '.vec')
+    logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
 
-program = os.path.basename(sys.argv[0])
-logging.info("finished running %s" % program)
+    # check and process input arguments
+    if len(sys.argv) < 4:
+        print(globals()['__doc__'] % locals())
+        sys.exit(1)
+    inp, outp1, outp2 = sys.argv[1:4]
+    train_file = inp
+    train_corpus = list(read_corpus(train_file))
+    model = Doc2Vec(vector_size=100, min_count=1, window=2, workers=multiprocessing.cpu_count())
+    model.build_vocab(train_corpus)
+    model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+    model.save(outp1)
+    # model.wv.save(outp2, binary=False)
+# lee_train_file = '../data/raw/SemEval2010_train_raw.txt'
+# lee_test_file = '../data/SemEval2010/train/C-41.txt.final'
+#
+#
+#
+# train_corpus = list(read_corpus(lee_train_file))
+# test_corpus = list(read_corpus(lee_test_file, tokens_only=True))
+# model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40)
+# model.build_vocab(train_corpus)
+# model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+# model.save(outp1)
+# model.wv.save_word2vec_format(outp2, binary=False)
+# print(model.infer_vector(['only', 'you', 'can', 'prevent', 'forest', 'fires']))
