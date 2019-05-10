f8b6790acaa5e11a5bd5076ccb3aa82de456a7f7
NoteXYX <707997228@qq.com>
2019-05-10 10:17:34 +0800

    2019.5.9 14:21

--- scripts/myjieba.py
+++ scripts/myjieba.py
@@ -2,15 +2,16 @@ import jieba
 
 def jieba1():
     try:
-        f1 = open('D:\PycharmProjects\Dataset\keywordEX\patent\_all_rm_abstract_NEW.txt', 'r', encoding='utf-8')
-        f2 = open(r'D:\PycharmProjects\Dataset\keywordEX\patent\all_fc_abstract_NEW.txt', 'w', encoding='utf-8')
+        f1 = open('D:\PycharmProjects\Dataset\keywordEX\patent\_qingjie_abstract.txt', 'r', encoding='utf-8')
+        f2 = open(r'D:\PycharmProjects\Dataset\keywordEX\patent\6种专利摘要各100未标注\qingjie.txt', 'w', encoding='utf-8')
         mystr = f1.readlines()
         iters = 1
         for line_str in mystr:
-            seg_list = jieba.cut(line_str)
-            result = ' '.join(seg_list)
-            f2.write(result)
-            print('处理完成%d行' % (iters))
+            # seg_list = jieba.cut(line_str)
+            # result = ' '.join(seg_list)
+            f2.write(line_str)
+            f2.write('keywords:\n\n')
+            print('处理完成%d行' % iters)
             iters += 1
         # print(mystr[0])
 
@@ -47,4 +48,4 @@ def jieba2():
             f2.close()
 
 if __name__ == '__main__':
-    jieba2()
+    jieba1()
